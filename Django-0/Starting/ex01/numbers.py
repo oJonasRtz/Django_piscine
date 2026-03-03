8de1f002537d07a427faa02e96771e37e9ebdcc2
Jonas <jonasper19@gmail.com>
2026-03-03 17:28:25 -0300

    ex01 update + ex02

--- Django-0/Starting/ex01/numbers.py
+++ Django-0/Starting/ex01/numbers.py
@@ -2,13 +2,21 @@
 #'with' manages the opening e closing, preventing memory leaks
 #'strip()' === 'trim()' in other languages, removes leading and trailing whitespace
 
-def main() -> None:
-	
-	with open('numbers.txt', 'r') as file:
+def print_line(line: str) -> None:
+	val = line.split(',');
+	for n in val:
+		print(n.strip());
+
+def	readNumbers(fileName: str) -> None:
+	with open(fileName, 'r') as file:
 		for line in file:
-			numbersList = line.split(',');
-			for n in numbersList:
-				print(n.strip());
+			print_line(line);
+
+def main() -> None:
+	try:
+		readNumbers('numbers.txt');
+	except Exception as e:
+		print(f"An error occurred: {e}");
 
 if __name__ == '__main__':
 	main();
