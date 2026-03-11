4785a2abccf923070bf1e9352d8438383e69577a
oJonasRtz <jonasper19@gmail.com>
2026-03-11 17:41:38 -0300

    periodic table update

--- Django-0/Starting/ex07/periodic_table.py
+++ Django-0/Starting/ex07/periodic_table.py
@@ -21,35 +21,96 @@ def listToDict(l: list[str]) -> dict[str, dict[str]]:
 
 	return d
 
+def build_css(title: str) -> str:
+	if not isinstance(title, str):
+		raise ValueError("__TYPE_ERROR__")
+
+	title = title.split('.')[0] + '.css'
+	
+	doc = """
+	body {
+		background-color: #0b1d3a;
+		color: white;
+		font-family: Arial, sans-serif;
+	}
+	table {
+		border-collapse: collapse;
+		margin: auto;
+	}
+	td {
+		padding: 10px;
+		text-align: center;
+	}
+	h4 {
+		margin: 5px;
+	}
+	ul {
+		list-style: none;
+		padding: 0;
+	}
+	td:hover {
+		scale: 1.1;
+		transition: all 0.3s ease;
+		border: 1px solid white;
+	}
+	"""
+ 
+	with open(title, 'w') as file:
+		file.write(doc)
+
+	return title
+ 
+def build_table(data) -> str:
+	doc = ''
+	col = 0
+ 
+	doc += '<tr>'
+	
+	for key, value in data.items():
+		pos = int(value['position'])
+
+		while col < pos:
+			doc += '<td></td>'
+			col += 1
+
+		doc += f'''
+			<td style="border: 1px solid white; padding:10px; background-color: #1f3b73;">
+				<h4>{key}</h4>
+				<ul>
+					<li>No {value['number']}</li>
+					<li>{value['small']}</li>
+					<li>{value['molar']}</li>
+					<li>{value['electron']} electron</li>
+				</ul>
+			</td>
+		'''
+  
+		col += 1
+		if col > 17:
+			doc += '</tr>\n<tr>'
+			col = 0
+  
+	doc += '</tr>'
+	return doc
+
 def build_html(filename: str, dict: dict[str, dict[str]]) -> None:
 	title = filename.split('.')[0].replace('_', ' ').title()
+
+	css = build_css(filename)
 
 	head = f"""
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>{title}</title>
+		<link rel="stylesheet" href="{css}">
 	"""
 
+
 	body = f"""
-		<table>
-					{''.join(
-						f'''
-						<tr>
-							<td style="border: 1px solid black; padding:10px">
-								<h4>{key}</h4>
-								<ul>
-									<li>No {value['number']}</li>
-									<li>{value['small']}</li>
-									<li>{value['molar']}</li>
-									<li>{value['electron']} electron</li>
-								</ul>
-							</td>
-						</tr>
-						'''
-						for key, value in dict.items()
-					)}
+		<table style="border-collapse: collapse;">
+			{build_table(dict)}
 		</table>
-	"""
+ """
 
 	doc = f"""<!DOCTYPE html>
 	<html lang="en">
@@ -58,6 +119,13 @@ def build_html(filename: str, dict: dict[str, dict[str]]) -> None:
 		</head>
 		<body>
 			{body}
+			<p>
+				<a href="https://jigsaw.w3.org/css-validator/check/referer">
+					<img style="border:0;width:88px;height:31px"
+						src="https://jigsaw.w3.org/css-validator/images/vcss"
+						alt="Valid CSS!" />
+				</a>
+			</p>
 		</body>
 	</html>
 """
